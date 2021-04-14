a76c35237ebfaa202fd8a0a33ce8f36dcf727569
gameybobcat622 <79866297+gameybobcat622@users.noreply.github.com>
2021-04-14 09:13:00 -0700

    Delete dee fuuk.py

--- dee fuuk.py
+++
@@ -1,9 +0,0 @@
-def factorial(x):
-    if x == 1 :
-        return 1
-    else:
-        return(x*factorial(x-1))
-
-if __name__ == "__main__":
-    num = input("what number would you like the factorial of?  ")
-    print("The factorial of ", num, " is ", factorial(int(num)))
